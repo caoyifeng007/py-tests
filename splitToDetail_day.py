9a2b886f88da0e93548b4613c518fe6c8fb7b558
ElvinsJ <caoyifeng007@163.com>
2022-07-15 14:59:58 +0800

    bug fix

---
+++ splitToDetail_day.py
@@ -0,0 +1,89 @@
+import csv
+import re
+import os
+import sys
+import argparse
+
+
+def getExtension(f):
+    return str(os.path.basename(f)).split('.')[-1]
+
+
+def getBareName(f):
+    return str(os.path.basename(f)).split('.')[0]
+
+
+parser = argparse.ArgumentParser(description='行情云历史数据拆分脚本')
+parser.add_argument("path", type=str, help="specify a full path. eg: a/b.csv")
+parser.add_argument(
+    "-d", "--folder", help="specify a folder.", action="store_true")
+args = parser.parse_args()
+
+inputFiles = []
+if not args.path:
+    print("Please specify a file or folder.")
+    sys.exit()
+
+if args.folder:
+    if os.path.isdir(args.path):
+        for root, _, fileNames in os.walk(args.path):
+            for file in fileNames:
+                if getExtension(file) == 'csv':
+                    inputFiles.append(os.path.join(root,  file))
+    else:
+        print(f"The {args.path} is not a folder")
+        sys.exit()
+
+else:
+    if os.path.isfile(args.path):
+        inputFiles.append(args.path)
+    else:
+        print(f"The {args.path} is not a file")
+        sys.exit()
+
+print("Input files:")
+print(inputFiles)
+
+# DCE 品种 才能用这个正则
+contractReg = re.compile('([a-z]+)(\d{4})')
+for file in inputFiles:
+
+    eFile = open(file)
+    eReader = csv.reader(eFile)
+
+    for row in eReader:
+
+        date = getBareName(file).split('_')[-1]
+        exchange = str(list(row)[6]).split('_')[0]
+        product = str(list(row)[6]).split('_')[-1]
+        pathArr = [exchange, date, 'snap', product]
+
+        contract = str(list(row)[7])
+
+        if ("-P-" in contract) or ("-C-" in contract):
+            pathArr.append('opt')
+        else:
+            pathArr.append('ftr')
+
+        mo = contractReg.search(contract)
+        item, n = mo.groups()
+        pathArr.append(item)
+
+        outputPath = os.path.sep.join(pathArr)
+        outputName = '_'.join(pathArr)+'.csv'
+
+        if not os.path.exists(outputPath):
+            os.makedirs(outputPath)
+
+        # https://docs.python.org/zh-cn/3/library/csv.html#id3
+        # 如果没有指定 newline=''，则嵌入引号中的换行符将无法正确解析，并且在写入时，使用 \r\n 换行的平台会有多余的 \r 写入。
+        # 由于 csv 模块会执行自己的（通用）换行符处理，因此指定 newline='' 应该总是安全的。
+        outputFile = open(
+            os.path.join(outputPath, outputName),
+            'w',
+            newline='')
+        outputWriter = csv.writer(outputFile)
+        outputWriter.writerow(row)
+        outputFile.close()
+
+    eFile.close()
--- splitToDetail_snap.py
+++ splitToDetail_snap.py
@@ -5,12 +5,12 @@ import sys
 import argparse
 
 
-def getExtension(file):
-    return str(os.path.basename(file)).split('.')[-1]
+def getExtension(f):
+    return str(os.path.basename(f)).split('.')[-1]
 
 
-def getBareName(file):
-    return str(os.path.basename(file)).split('.')[0]
+def getBareName(f):
+    return str(os.path.basename(f)).split('.')[0]
 
 
 parser = argparse.ArgumentParser(description='行情云历史数据拆分脚本')
@@ -29,7 +29,7 @@ if args.folder:
         for root, _, fileNames in os.walk(args.path):
             for file in fileNames:
                 if getExtension(file) == 'csv':
-                    inputFiles.append(os.path.join(root,  file))
+                    inputFiles.append(os.path.join(root, file))
     else:
         print(f"The {args.path} is not a folder")
         sys.exit()
@@ -48,7 +48,7 @@ print(inputFiles)
 contractReg = re.compile('([a-z]+)(\d{4})')
 for file in inputFiles:
 
-    eFile = open(file)
+    eFile = open(file, encoding='utf-8')
     eReader = csv.reader(eFile)
 
     for row in eReader:
@@ -69,15 +69,23 @@ for file in inputFiles:
         item, n = mo.groups()
         pathArr.append(item)
 
-        outputPath = os.path.sep.join(pathArr)
-        outputName = '_'.join(pathArr)+'.csv'
+        outputPath = os.path.abspath(os.path.sep.join(pathArr))
+        outputName = '_'.join(pathArr) + '.csv'
 
-        if not os.path.exists(outputPath):
+        # w和a模式都会自动创建新文件,但是路径需要手动创建
+        if os.path.exists(outputPath):
+            mode = 'a'
+        else:
+            mode = 'w'
             os.makedirs(outputPath)
 
+        # https://docs.python.org/zh-cn/3/library/csv.html#id3
+        # 如果没有指定 newline=''，则嵌入引号中的换行符将无法正确解析，并且在写入时，使用 \r\n 换行的平台会有多余的 \r 写入。
+        # 由于 csv 模块会执行自己的（通用）换行符处理，因此指定 newline='' 应该总是安全的。
         outputFile = open(
             os.path.join(outputPath, outputName),
-            'w',
+            mode,
+            encoding='utf-8',
             newline='')
         outputWriter = csv.writer(outputFile)
         outputWriter.writerow(row)
