0fe1b99ba96178cbdd5a70481af7bba4add76954
ElvinsJ <caoyifeng007@163.com>
2022-07-19 15:57:59 +0800

    添加日终逻辑

--- splitToDetail_day.py
+++ splitToDetail_day.py
@@ -1,8 +1,8 @@
-import csv
-import re
-import os
-import sys
 import argparse
+import csv
+import os
+import re
+import sys
 
 
 def getExtension(f):
@@ -29,7 +29,7 @@ if args.folder:
         for root, _, fileNames in os.walk(args.path):
             for file in fileNames:
                 if getExtension(file) == 'csv':
-                    inputFiles.append(os.path.join(root,  file))
+                    inputFiles.append(os.path.join(root, file))
     else:
         print(f"The {args.path} is not a folder")
         sys.exit()
@@ -48,17 +48,17 @@ print(inputFiles)
 contractReg = re.compile('([a-z]+)(\d{4})')
 for file in inputFiles:
 
-    eFile = open(file)
+    eFile = open(file, encoding='utf-8')
     eReader = csv.reader(eFile)
 
+    fileDict = dict()
     for row in eReader:
 
+        contract = str(list(row)[7])
         date = getBareName(file).split('_')[-1]
         exchange = str(list(row)[6]).split('_')[0]
         product = str(list(row)[6]).split('_')[-1]
-        pathArr = [exchange, date, 'snap', product]
-
-        contract = str(list(row)[7])
+        pathArr = [exchange, date, 'day', product]
 
         if ("-P-" in contract) or ("-C-" in contract):
             pathArr.append('opt')
@@ -69,21 +69,25 @@ for file in inputFiles:
         item, n = mo.groups()
         pathArr.append(item)
 
-        outputPath = os.path.sep.join(pathArr)
-        outputName = '_'.join(pathArr)+'.csv'
+        outputPath = os.path.abspath(os.path.sep.join(pathArr))
+        outputName = '_'.join(pathArr) + '.csv'
 
         if not os.path.exists(outputPath):
             os.makedirs(outputPath)
 
-        # https://docs.python.org/zh-cn/3/library/csv.html#id3
-        # 如果没有指定 newline=''，则嵌入引号中的换行符将无法正确解析，并且在写入时，使用 \r\n 换行的平台会有多余的 \r 写入。
-        # 由于 csv 模块会执行自己的（通用）换行符处理，因此指定 newline='' 应该总是安全的。
-        outputFile = open(
-            os.path.join(outputPath, outputName),
-            'w',
-            newline='')
+        writingFile = os.path.join(outputPath, outputName)
+        if writingFile not in fileDict:
+            fileDict[writingFile] = dict()
+
+        fileDict[writingFile][contract] = row
+
+    for f, cs in fileDict.items():
+        outputFile = open(f, 'w', encoding='utf-8', newline='')
         outputWriter = csv.writer(outputFile)
-        outputWriter.writerow(row)
+
+        for r in cs.values():
+            outputWriter.writerow(r)
+
         outputFile.close()
 
     eFile.close()
